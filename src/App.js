f1e7d06750c9cef4cf32f5949610aa227ccf8fb9
YooUisun <yoouisun33@naver.com>
2025-06-04 03:00:18 +0900

    스크롤 추가가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 import Main from "./Main/Main";
-import SubMain from "./SubMain/SubMain";
 import Work from "./Work/Work";
 import WorkDetail from "./WorkDetail/WorkDetail";
 import About from "./About/About";
@@ -11,7 +10,7 @@ import Contact from "./Contact/Contact";
 import News from "./News/News";
 
 import NavBar from "./NavBar/NavBar";
-import Footer from "./Footer/Footer";  // Footer import 추가
+import Footer from "./Footer/Footer";
 
 import "./App.css";
 
@@ -21,18 +20,15 @@ export default function App() {
 
   return (
     <>
-      {!isMainPage && <NavBar />} {/* Main 제외한 페이지에만 NavBar */}
+      {!isMainPage && <NavBar />}
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="sub" element={<SubMain />} />
-        <Route path="work" element={<Work />} />
-        <Route path="work/:id" element={<WorkDetail />} />
-        <Route path="news" element={<News />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/work/:id" element={<WorkDetail />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-
-      {/* Main 페이지가 아닐 때만 Footer 보여주기 */}
       {!isMainPage && <Footer />}
     </>
   );
