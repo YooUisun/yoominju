f8d3f75263a4b83e7de59ac4cb558674e2d97194
YooUisun <yoouisun33@naver.com>
2025-07-10 17:01:09 +0900

    오디오

--- src/WorkDetail/WorkDetail.js
+++ src/WorkDetail/WorkDetail.js
@@ -549,7 +549,7 @@ export default function WorkDetail() {
                         style={{ border: 'none', background: 'none', cursor: 'pointer', padding: 0 }}
                       >
                         <img
-                          src={process.env.PUBLIC_URL + "/img/play.png"}
+                          src={process.env.PUBLIC_URL + "/img/Audio/play.png"}
                           alt="Play"
                           style={{ width: '24px', height: '24px' }}
                         />
@@ -565,7 +565,7 @@ export default function WorkDetail() {
                         style={{ border: 'none', background: 'none', cursor: 'pointer', padding: 0 }}
                       >
                         <img
-                          src={process.env.PUBLIC_URL + "/img/play-pause.png"}
+                          src={process.env.PUBLIC_URL + "/img/Audio/play-pause.png"}
                           alt="Stop"
                           style={{ width: '30px', height: '30px' }}
                         />
